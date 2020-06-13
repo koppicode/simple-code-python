11e0b0daba92fff9c7965230d41214418c85a705
koppicode <66888813+koppicode@users.noreply.github.com>
2020-06-14 06:25:24 +0700

    First Commit SimpleCode.py

---
+++ simpleCode.py
@@ -0,0 +1,34 @@
+#Simple Code Python
+
+print('Hello word')
+
+print(30*'=')
+
+variable1 = 'ini adalah contoh variabel1' #untuk string
+variable2 = 34 #untuk integer
+variable3 = 3.14 #untuk float
+
+print('ini adalah variable 1 :', variable1, type(variable1))
+print('ini adalah variable 2 :', variable2, type(variable2))
+print('ini adalah variable 3 :', variable3, type(variable3))
+
+#list,tuple
+Ganjil = [1,3,5,7,9] #list
+Genap = (2,4,6,8,10) #tuple
+
+print('ini bilangan ganjil :',Ganjil, type(Ganjil))
+print('ini bilangan genap :',Genap, type(Genap))
+
+#for loop
+
+Buahan = ['Apel','Jeruk','Melon','Semangka','Pepaya','Anggur','Kelengkeng']
+
+for i in Buahan:
+    print(i)
+    
+ #fungsi
+ 
+ def ContohFungsi():
+    print('Ini Adalah Fungsi')
+
+
